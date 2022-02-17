77bf78dc413200d7da81dbc3a9cfae96394b760e
Kiktor <kania.wiktorr@gmail.com>
2022-02-17 19:57:30 +0100

    Fajnie poukładane foldery :)

---
+++ GraKonkursowa/Assets/Scripts/MapGeneration/RandomWalk.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class NewBehaviourScript
+{
+    public static HashSet<Vector2> RandomWalk(Vector2 startPos, int walkLength)
+    {
+        HashSet<Vector2> path = new HashSet<Vector2>();
+
+        path.Add(startPos);
+        var previousPos = startPos;
+    }
+}
